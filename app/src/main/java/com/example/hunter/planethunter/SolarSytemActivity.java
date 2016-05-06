f519f42d8c37ebbb3e0111af59ae0b77e61e4dce
IgorJakovljevic <igor.james.jakovljevic@gmail.com>
2016-05-06 13:48:46 +0200

    [IJ,JB] Created overview activity and solar system activity

---
+++ app/src/main/java/com/example/hunter/planethunter/SolarSytemActivity.java
@@ -0,0 +1,19 @@
+package com.example.hunter.planethunter;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class SolarSytemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_solar_sytem);
+
+    }
+
+}
