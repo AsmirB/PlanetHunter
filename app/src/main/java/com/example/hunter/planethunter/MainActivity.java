318553c4b13c7c1433edf5b813eb2d9b5c2fc78b
IgorJakovljevic <igor.james.jakovljevic@gmail.com>
2016-05-20 15:23:36 +0200

    [IJ, JB]Implemented planet activity_planet and created mock event handlers.

--- app/src/main/java/com/example/hunter/planethunter/MainActivity.java
+++ app/src/main/java/com/example/hunter/planethunter/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.hunter.planethunter;
 
+import android.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openSolarSystem(View view) {
+        // Do something in response to button click
+        new AlertDialog.Builder(this)
+                .setTitle("Your Alert")
+                .setMessage("Your Message")
+                .setCancelable(false).create().show();
     }
 }
---
+++ app/src/main/java/com/example/hunter/planethunter/SolarSystem.java
@@ -0,0 +1,13 @@
+package com.example.hunter.planethunter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SolarSystem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_solar_system);
+    }
+}
